a5d67c1ef9ec4fe05a875f4a61928a6ae4cead52
Jimpa Jamtsho <jamtshojimpa@gmail.com>
2025-07-09 15:31:27 +0600

    Add validation and error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -15,7 +15,7 @@ export class TasksService {
     getTaskById(id: string): Task {
         const found = this.tasks.find((task)=> task.id === id);
         if(!found){
-            throw new Error(`Task with ID "${id}" not found`);
+            throw new NotFoundException;
         }
         return found;
     }
@@ -34,7 +34,8 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter((task)=> task.id === id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task)=> task.id === found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
